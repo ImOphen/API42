e795b82a2810c12706027ed01aa9982d1e405263
Ophen <43254081+ImOphen@users.noreply.github.com>
2023-02-20 17:46:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from distutils.core import setup
 setup(
-  name = 'API42',         # How you named your package folder (MyLib)
-  packages = ['API42'],   # Chose the same as "name"
-  version = '0.0.4',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Wrapper for the 42 intra API',   # Give a short description about your library
-  author = 'Ayman Touhami',                   # Type in your name
-  author_email = 'itsophen@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/ImOphen/API42',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ImOphen/API42/archive/refs/tags/v0.0.4.tar.gz',    # I explain this later on
-  keywords = ['42', 'Intra', 'API'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'API42',
+  packages = ['API42'],
+  version = '0.0.4',
+  license='MIT',
+  description = 'Wrapper for the 42 intra API',
+  author = 'Ayman Touhami',
+  author_email = 'itsophen@gmail.com',
+  url = 'https://github.com/ImOphen/API42',
+  download_url = 'https://github.com/ImOphen/API42/archive/refs/tags/v0.0.4.tar.gz',
+  keywords = ['42', 'Intra', 'API'],
+  install_requires=[ 
           'requests',
       ],
   classifiers=[
-    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 4 - Beta',    
+    'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',  
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
