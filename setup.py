13df8053ad4c146a1f9ecf9c62d796da053577d4
Ophen <itsophen@gmail.com>
2023-02-20 20:48:31 +0100

    Fix

--- setup.py
+++ setup.py
@@ -27,6 +27,5 @@ setup(
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
-    'Programming Language :: Python :: 3'
   ],
 )
